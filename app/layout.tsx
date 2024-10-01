eac7e3d43ffa2057831c2b5673daecb982b8fc39
Anish Araz <anisharaz919@gmail.com>
2024-10-01 17:54:13 +0530

    FEAT: add Metadata & robots.txt file

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,32 @@ export const metadata: Metadata = {
   title: "BlockX3",
   description:
     "Turning Ideas into Reality: Open-Source Innovation Through Community Collaboration",
+  keywords:
+    "blockchain, open-source,development, innovation, community, collaboration",
+  openGraph: {
+    images: [
+      {
+        url: "https://static.aaraz.me/blockx3/blockx3_blue.jpg",
+        type: "image/jpeg",
+        height: 804,
+        width: 630,
+        alt: "BlockX3",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@block_x3",
+    images: [
+      {
+        url: "https://static.aaraz.me/blockx3/blockx3_blue.jpg",
+        type: "image/jpeg",
+        height: 804,
+        width: 630,
+        alt: "BlockX3",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
