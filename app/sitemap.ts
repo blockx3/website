b04eabe6c52f05b4d94d689bad332a28266f80a6
Nawin Kumar Sharma <126372969+NawinKumarSharma@users.noreply.github.com>
2024-10-02 17:56:16 +0530

    feat: add sitemap for better SEO (#25)
    * feat: add sitemap & robots file for better SEO

---
+++ app/sitemap.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from 'next';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = 'https://blockx3.xyz';
+
+  // add some more sitemap logic
+  const staticPages = [''].map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  return [...staticPages]; // if there will be any dynamic page . remind me, i will update
+}
