8f2c07926b8d7231a0f10a3b86d2921c5699831c
Nawin <nawinsharma60@gmail.com>
2024-10-02 17:33:14 +0530

    feat: remove the robot.txt and update the sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,8 @@ import { MetadataRoute } from 'next';
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = 'https://blockx3.xyz';
 
-  const staticPages = ['', '/'].map((route) => ({
+  // add some more sitemap logic
+  const staticPages = [''].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString(),
   }));
